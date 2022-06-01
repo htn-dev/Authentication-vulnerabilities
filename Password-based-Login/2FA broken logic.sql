bedd931cbb4b84a74dfb23ba5f91a5d559bb4401
HTN <96134288+htn-dev@users.noreply.github.com>
2022-06-01 18:46:06 -0400

    Update 2FA broken logic.sql

--- Password-based-Login/2FA broken logic.sql
+++ Password-based-Login/2FA broken logic.sql
@@ -1,2 +1,5 @@
+--Sometimes flawed logic in two-factor authentication means that after a user has completed the initial login step, 
+--the website doesn't adequately verify that the same user is completing the second step. 
+
 --With Burp running, log in to your own account and investigate the 2FA verification process. 
 --Notice that in the POST /login2 request, the verify parameter is used to determine which user's account is being accessed.
