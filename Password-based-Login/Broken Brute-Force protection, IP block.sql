cfe151ac99e3a96f6cd756478c0ec555a133be6f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-06-12 11:37:06 -0400

    Update Broken Brute-Force protection, IP block.sql

--- Password-based-Login/Broken Brute-Force protection, IP block.sql
+++ Password-based-Login/Broken Brute-Force protection, IP block.sql
@@ -12,4 +12,4 @@
 --There should only be a single 302 response for requests with the username carlos. Make a note of the password from the Payload 2 column.
 --Log in to Carlos's account using the password that you identified and access his account page to solve the lab.
  
- 
+  
