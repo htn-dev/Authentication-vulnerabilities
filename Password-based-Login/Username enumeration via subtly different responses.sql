d216cdb43982dfa1d663a83a4e29dc74f3430a4f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-07-18 13:03:58 -0400

    Update Username enumeration via subtly different responses.sql

--- Password-based-Login/Username enumeration via subtly different responses.sql
+++ Password-based-Login/Username enumeration via subtly different responses.sql
@@ -5,8 +5,7 @@
 --When the attack is finished, notice that there is an additional column containing the error message you extracted. Sort the results using this column to notice 
 --that one of them is subtly different.
 --Look closer at this response and notice that it contains a typo in the error message - instead of a full stop/period, there is a trailing space. Make a note of 
---this username.
-    and go back to the Positions tab. Insert the username you just identified and add a payload position to the password parameter:
+--this username and go back to the Positions tab. Insert the username you just identified and add a payload position to the password parameter:
 username=identified-user&password=§invalid-password§
 --On the Payloads tab, clear the list of usernames and replace it with the list of passwords. Start the attack.
 --When the attack is finished, notice that one of the requests received a 302 response. Make a note of this password.
